61bbb379c91f7f54bbc8cdb5ad9d50b5dde9dd05
Paul Jaramillo <pauljara14@gmail.com>
2023-05-15 10:26:50 -0500

    Listas Enlazada

--- aplicaciones_listas.py
+++ aplicaciones_listas.py
@@ -1,11 +1,45 @@
-# Pedimos al usuario que ingrese una lista de números separados por comas
-numeros_str = input("Ingresa una lista de números separados por comas: ")
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
-# Convertimos la cadena de entrada en una lista de números
-numeros = [float(x) for x in numeros_str.split(",")]
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def append(self, data):
+        node = Node(data)
+        if self.tail:
+            self.tail.next = node
+            self.tail = node
+        else:
+            self.head = node
+            self.tail = node
+    
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node.data
+            node = node.next
 
-# Calculamos el promedio de los números en la lista
-promedio = sum(numeros) / len(numeros)
+def promedio(lista):
+    suma = 0
+    n = 0
+    for num in lista:
+        suma += num
+        n += 1
+    if n == 0:
+        return None
+    else:
+        return suma / n
 
-# Mostramos el promedio en pantalla
-print("El promedio es:", promedio)
+# Ejemplo de uso
+lista = LinkedList()
+lista.append(2)
+lista.append(4)
+lista.append(6)
+lista.append(8)
+lista.append(10)
+
+print(promedio(lista))  # Imprime 6.0
